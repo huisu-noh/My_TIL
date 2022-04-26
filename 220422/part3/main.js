9296bb82db5fb8c0212466d75e35f7cb5fed353a
huisu <huisu0429@naver.com>
2022-04-26 13:05:31 +0900

    22.04.26 JavaScript Essentials
    Part 3

--- 220422/part3/main.js
+++ 220422/part3/main.js
@@ -1,8 +1,30 @@
 import getType from './getType';
 import random from './getRandom';
 
-const a = 1;
-const b = '1';
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name;
+    this.wheel = wheel;
+  }
+}
 
-console.log(a === b);
-console.log(a == b);
+const MyVehicle = new Vehicle('운송수단', 2);
+console.log(MyVehicle);
+
+// extends 확장, 상속
+class Bicycle extends Vehicle {
+  constructor(name, wheel) {
+    super(name, wheel);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(name, wheel, license) {
+    super(name, wheel);
+    this.license = license;
+  }
+}
+
+console.log(new Bicycle('삼천리', 2));
+console.log(new Bicycle('세발자전거', 3));
+console.log(new Car('기아', 4, '필요하다'));
