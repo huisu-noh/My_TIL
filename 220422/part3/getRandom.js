39e5e5ad1367561e350130dc176aea840db61c8b
huisu <huisu0429@naver.com>
2022-04-28 16:00:08 +0900

    JavaScript Essentials
    Part 3

--- 220422/part3/getRandom.js
+++ 220422/part3/getRandom.js
@@ -1,3 +1,13 @@
-export default function random() {
+// 여러번 사용이 가능하다
+
+export function random() {
   return Math.floor(Math.random() * 10);
 }
+
+export const user = {
+  name: "huisu",
+  age: 80,
+};
+
+// 같이 사용 가능
+export default 123;
