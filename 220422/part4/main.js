36fa70533262c597128923dd21c8d86a27afcf5c
huisu <huisu0429@naver.com>
2022-04-28 16:05:39 +0900

    JavaScript Level up
    part4

---
+++ 220422/part4/main.js
@@ -0,0 +1,15 @@
+//import _ from "lodash"; // From 'node_modules!'
+//import myData from "./MyData.json";
+import axios from "axios";
+
+function fetchMovies() {
+  axios.get("https://www.omdbapi.com/?apikey=7035c60c&s=frozen").then((res) => {
+    console.log(res);
+    const h1El = document.querySelector("h1");
+    const imgEl = document.querySelector("img");
+    h1El.innerText = res.data.Search[0].Title;
+    imgEl.src = res.data.Search[0].Poster;
+  });
+}
+
+fetchMovies();
