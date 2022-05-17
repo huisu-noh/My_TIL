0b54e90df3315bd1d5e9d77544db3a5b5dfaddd6
huisu <huisu0429@naver.com>
2022-05-17 15:24:54 +0900

    Moment Example

---
+++ 220513/Moment/MomentExample.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useState } from 'react';
+// import moment from 'moment';
+import moment from 'moment-timezone';
+import 'moment/locale/ko';
+
+export default function MomentExample() {
+  const birthDayRef = useRef(null);
+  const [day, setDay] = useState('');
+  const handleBirthDayChange = (event) => {
+    setDay(moment(event.target.value, 'YYYY-MM-DD').format('dddd'));
+  };
+
+  const momentDate = moment();
+  const newMomentDate = momentDate.add(1, 'week');
+  const cloneNewMomentDate = newMomentDate.clone().add(1, 'week');
+  return (
+    <div>
+      <h1>Moment</h1>
+      <div>Immutable Check</div>
+      <div>{momentDate.format()}</div>
+      <br />
+      <div>{newMomentDate.format()}</div>
+      <br />
+      <div>{cloneNewMomentDate.format()}</div>
+      <br />
+      <div>Summer Time (New-york)</div>
+      <div>
+        2018년 3월 10일 13시에 하루 더하기:
+        {moment
+          .tz('2018-03-10 13:00:00', 'America/New_York')
+          .add(1, 'day')
+          .format()}
+      </div>
+      <div>
+        2018년 3월 10일 13시에 24시간 더하기:
+        {moment
+          .tz('2018-03-10 13:00:00', 'America/New_York')
+          .add(24, 'hour')
+          .format()}
+      </div>
+      <br />
+      <div>Leap year(Korea)</div>
+      <div>
+        2017년 1월 1일에 1년 빼기:
+        {moment('2017-01-01').subtract(1, 'year').format()}
+      </div>
+      <div>
+        2017년 1월 1일에 365일 빼기:
+        {moment('2017-01-01').subtract(365, 'day').format()}
+      </div>
+      <br />
+      <div>한국어로 표기 (05-16-2022를 2022년 5월 16일로 표기)</div>
+      <div>{moment('05-16-2022').format('YYYY년 M월 DD일')}</div>
+      <br />
+      <div>두 날짜가 비교</div>
+      <div>
+        <input type='date' ref={birthDayRef} onChange={handleBirthDayChange} />
+        <div>무슨 요일이었을까?</div>
+        <div>{day}</div>
+      </div>
+      <br />
+      <div>두 날짜 비교</div>
+      <div>2021-07-17 03:00 와 2021-07-18 02:00는 몇시간 차이인가?</div>
+      <div>
+        {`${moment('2021-07-17 03:00').diff(
+          moment('2021-07-18 02:00'),
+          'hours'
+        )}
+        시간`}
+      </div>
+      <br />
+    </div>
+  );
+}
